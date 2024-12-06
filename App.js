bd6598bdcdb70203aab37ef91674aa19c2524dc3
Srivastavaaman8604 <aman.srivastava2639@gmail.com>
2024-12-06 15:42:51 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,105 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { View, Image, StyleSheet, TouchableOpacity, Text, FlatList, Button } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [data, setData] = useState([])
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // Define the async function
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://fakestoreapi.com/products');
+        setData(response.data); // Save fetched data in state
+      } catch (err) {
+        setError(err.message); // Save error message in state
+      }
+    };
+
+    // Call the function
+    fetchData();
+  }, []); 
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.head}>PRODUCTS</Text>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Image source={{uri: item.image}} style={styles.image}/>
+            <Text style={styles.title}>{item.title.substring(0,20)}</Text>
+            <Text>${item.price}</Text>
+            <View style={styles.button}>
+              <TouchableOpacity style={styles.btn}>
+                <Text style={styles.text}>Add to Cart</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.btn}>
+                <Text style={styles.text}>Buy Now</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        )}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    marginTop: 50,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  item: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 10,
+    resizeMode: 'contain',
+  },
+  head: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  price: {
+    fontSize: 16,
+    color: '#28a745',
+    fontWeight: 'bold',
+    marginLeft: 10,
+  },
+  btn: {
+    backgroundColor: '#007BFF',
+    justifyContent: 'space-between',
+    marginTop: 10,
+    padding: 10,
+    borderRadius: 10,
+    marginRight: 10,
+  },
+  text:{
+    color: 'white',
+    fontSize: 16,
+  },
+  button:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+    
   },
 });
+
+export default App;
